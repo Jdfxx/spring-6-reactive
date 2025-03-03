642d70b80359f292dc35f5df157c91fa6ccdd426
Filip <fhagno@yahoo.com>
2025-03-03 14:35:46 +0100

    Init

---
+++ src/test/java/pl/filiphagno/spring6reactive/Spring6ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filiphagno.spring6reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
